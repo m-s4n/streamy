e17b856ee4021c3ff8ae0e53862643cefc5c50e9
Mustafa SEYMEN <sevgi.gmail.com>
2020-07-31 21:21:53 +0300

    completed router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {Route, Switch } from 'react-router-dom';
+import StreamCreate from './components/streams/StreamCreate';
+import StreamEdit from './components/streams/StreamEdit';
+import StreamDelete from './components/streams/StreamDelete';
+import StreamShow from './components/streams/StreamShow';
+import StreamList from './components/streams/StreamList';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div>
-      App
+    <div className="ui container">
+      <Header/>
+      <Switch>
+      <Route path="/" exact component={StreamList} />
+      <Route path="/stream/new" exact component={StreamCreate} />
+      <Route path="/stream/edit" exact component={StreamEdit} />
+      <Route path="/stream/delete" exact component={StreamDelete} />
+      <Route path="/stream/show" exact component={StreamShow} />
+    </Switch>
     </div>
   );
 }
